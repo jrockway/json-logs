4a14acbfe24e5b6522ef2113d310409302b0a99b
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-08-09 19:32:42 -0400

    main: allow strict mode to be selected from the command-line

--- cmd/jlog/main.go
+++ cmd/jlog/main.go
@@ -18,6 +18,7 @@ type general struct {
 	RelativeTimestamps bool   `long:"relative" short:"r" description:"Print timestamps as a duration since the program started instead of absolute timestamps."`
 	AbsoluteEvery      int    `long:"absolute_every" description:"A compromise between relative and absolute timestamps; output an absolute timestamp, and then a duration relative to that timestamp until --absolute_every lines have been printed.  Implies --relative_timestamps." default:"0"`
 	TimeFormat         string `long:"time_format" short:"t" description:"A go time.Format string describing how to format timestamps; or 'RFC3339'." default:"RFC3339"`
+	Lax                bool   `long:"lax" description:"If true, suppress any validation errors including non-JSON log lines and missing timestamps, levels, and message.  We extract as many of those as we can, but if something is missing, the errors will be silently discarded."`
 }
 
 type inputFormat struct {
@@ -51,11 +52,11 @@ func main() {
 		gen.TimeFormat = ""
 	}
 	ins := &parse.InputSchema{
-		LevelKey:     inf.LevelKey,
-		MessageKey:   inf.MessageKey,
-		TimeKey:      inf.TimestampKey,
-		TimeFormat:   parse.DefaultTimeParser,
-		StrictObject: true,
+		LevelKey:   inf.LevelKey,
+		MessageKey: inf.MessageKey,
+		TimeKey:    inf.TimestampKey,
+		TimeFormat: parse.DefaultTimeParser,
+		Strict:     !gen.Lax,
 	}
 	wantColor := isatty.IsTerminal(os.Stdout.Fd()) && !gen.NoColor
 	outs := &parse.OutputSchema{
