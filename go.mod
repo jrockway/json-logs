f71448402f3285af52295989c6167342cb71c2cf
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-08-13 03:09:00 -0400

    test parsers

--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/logrusorgru/aurora/v3 v3.0.0
 	github.com/mattn/go-colorable v0.1.7
 	github.com/mattn/go-isatty v0.0.12
+	go.uber.org/zap v1.15.0
 )
---
+++ pkg/parse/default_parsers_test.go
@@ -0,0 +1,68 @@
+package parse
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"go.uber.org/zap/zapcore"
+)
+
+func TestDefaultTimeParser(t *testing.T) {
+	testData := []struct {
+		in      interface{}
+		want    time.Time
+		wantErr bool
+	}{
+		{int(1), time.Unix(1, 0), false},
+		{int64(1), time.Unix(1, 0), false},
+		{float64(1.1), time.Unix(1, 100000000), false},
+		{"1970-01-01T00:00:01.000Z", time.Unix(1, 0), false},
+		{"1970-01-01T04:00:01.000+04:00", time.Unix(1, 0), false},
+		{map[string]interface{}{"seconds": float64(123), "nanos": float64(456)}, time.Unix(123, 456), false},
+		{nil, time.Time{}, true},
+		{"1", time.Time{}, true},
+	}
+	for i, test := range testData {
+		got, err := DefaultTimeParser(test.in)
+		if err != nil && !test.wantErr {
+			t.Errorf("test %d: unexpected error: %v", i, err)
+		} else if err == nil && test.wantErr {
+			t.Errorf("test %d: expected error", i)
+		}
+		if diff := cmp.Diff(got, test.want, cmpopts.EquateApproxTime(time.Nanosecond)); diff != "" {
+			t.Errorf("test %d: %s", i, diff)
+		}
+	}
+}
+
+func TestDefaultLevelParser(t *testing.T) {
+	testData := []struct {
+		in      interface{}
+		want    Level
+		wantErr bool
+	}{
+		{zapcore.DebugLevel.CapitalString(), LevelDebug, false},
+		{[]byte(zapcore.DebugLevel.CapitalString()), LevelDebug, false},
+		{zapcore.InfoLevel.CapitalString(), LevelInfo, false},
+		{zapcore.WarnLevel.CapitalString(), LevelWarn, false},
+		{zapcore.ErrorLevel.CapitalString(), LevelError, false},
+		{zapcore.PanicLevel.CapitalString(), LevelPanic, false},
+		{zapcore.FatalLevel.CapitalString(), LevelFatal, false},
+		{zapcore.DPanicLevel.CapitalString(), LevelDPanic, false},
+		{"foo", LevelUnknown, false},
+		{42, LevelUnknown, true},
+	}
+	for i, test := range testData {
+		got, err := DefaultLevelParser(test.in)
+		if err != nil && !test.wantErr {
+			t.Errorf("test %d: unexpected error: %v", i, err)
+		} else if err == nil && test.wantErr {
+			t.Errorf("test %d: expected error", i)
+		}
+		if diff := cmp.Diff(got, test.want); diff != "" {
+			t.Errorf("test %d: %s", i, diff)
+		}
+	}
+}
