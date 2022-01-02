49577cea8fabbfefd1b0a04d4873860c25822cad
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-01-01 23:34:26 -0500

    main: buffer signal channel as per linter suggestion

--- cmd/jlog/main.go
+++ cmd/jlog/main.go
@@ -180,7 +180,7 @@ func main() {
 		PriorityFields: out.PriorityFields,
 	}
 
-	sigCh := make(chan os.Signal)
+	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGPIPE)
 	var nSignals int32
 	go func() {
