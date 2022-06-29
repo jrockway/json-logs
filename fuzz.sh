85e49c61a3593a33e2107f5ffc7b0e1e03e4e255
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-06-29 01:52:52 -0400

    fuzz: pass in fuzzing time with FUZZTIME

--- fuzz.sh
+++ fuzz.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+FUZZTIME=${FUZZTIME:-10s}
+
 for t in `go test -list Fuzz ./pkg/parse | egrep -v '^(ok|\?)'`; do
     echo $t
-    go test -fuzz="^${t}$" -fuzztime=10s ./pkg/parse
+    go test -fuzz="^${t}$" -fuzztime=$FUZZTIME ./pkg/parse
     echo "---"
 done;
